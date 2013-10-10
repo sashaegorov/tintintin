8e7a150fcae81a9a8ba207c162573a38ac6e3c0c
Sasha Egorov <a.a.egoroff@gmail.com>
2013-10-11 03:11:46 +0400

    Old files moved to separate repo

---
+++ web_r18n_steps.rb
@@ -0,0 +1,44 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+
+# Usage example:
+# I press localized "login.signin"[ within "form"]
+When /^(?:|I )press localized "([^\"]*)"(?: within "([^\"]*)")?$/ do |button, selector|
+  with_scope(selector) do
+    click_button(eval "R18n.t.#{button}")
+  end
+end
+
+# Usage example:
+# I follow localized "login.signout"[ within "div"]
+When /^(?:|I )follow localized "([^\"]*)"(?: within "([^\"]*)")?$/ do |link, selector|
+  with_scope(selector) do
+    click_link(eval "R18n.t.#{link}")
+  end
+end
+
+# Usage example:
+# I should see localized "title"[ within "h1"]
+Then /^(?:|I )should see localized "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, selector|
+  with_scope(selector) do
+    if page.respond_to? :should
+      # TODO: Aviod ugly and slow `eval' if possible
+      page.should have_content(eval "R18n.t.#{text}")
+    else
+      assert page.has_content?(eval "R18n.t.#{text}")
+    end
+  end
+end
+
+# Usage example:
+# I should not see localized "error"[ within "h1"]
+Then /^(?:|I )should not see localized "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, selector|
+  with_scope(selector) do
+    if page.respond_to? :should
+      page.should have_no_content(eval "R18n.t.#{text}")
+    else
+      assert page.has_no_content?(eval "R18n.t.#{text}")
+    end
+  end
+end
